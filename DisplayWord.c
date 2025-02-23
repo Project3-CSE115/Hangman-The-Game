3b61e96f3701f22bace6075dc932b2b933498bbe
Avi-lab24 <avishek.saha.241@northsouth.edu>
2025-02-23 23:38:25 +0600

    Update DisplayWord.c

--- DisplayWord.c
+++ DisplayWord.c
@@ -1,16 +1,29 @@
 void displayWord(const char *word, const int*guessed)
-{
- 
-for(int i = 0; i < strlen(word); i++)
 
- if (guessed[i])
-{
-printf("%c", word[i]);
-}
-else
- {
- printf("_ ");
- }
- printf("\n");
+    {
+
+        for(int i = 0; i < strlen(word); i++)
+
+    {
+
+        if (guessed[i])
+
+        {
+
+            printf("%c", word[i]);
+
+        }
+
+        else
+
+        {
+
+            printf("_ ");
+
+        }
+
+    }
+
+    printf("\n");
 
 }
