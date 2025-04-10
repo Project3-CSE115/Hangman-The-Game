764652520b1b63f8352f62e5a51a64ed759f3a2a
Priyonti Tabassum Haque <priyonti.haque.251@northsouth.edu>
2025-04-10 18:41:38 +0600

    Update Headers+Definitions.c

--- Hangman Code/Headers+Definitions.c
+++ Hangman Code/Headers+Definitions.c
@@ -1,10 +1,18 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
-#include <ctype.h>
+#include <stdbool.h>
+#include <stdlib.h>
 #include <time.h>
+#include <windows.h>
+#include <ctype.h>
+#include <string.h>
+#include <conio.h>
 
 #define MAX_TRIES 6
-#define WORD_COUNT 100
-
+#define MAX_WORDS 250
+#define MAX_LEVELS 5
+#define WORD_COUNT 10
+#define MAX_THEMES 5
+#define MAX_LENGTH 20
+#define MAX_GAMES 3
 
