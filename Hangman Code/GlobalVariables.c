0c849a820f9baccd420a0b6da44d160e5527fe03
Priyonti Tabassum Haque <priyonti.haque.251@northsouth.edu>
2025-04-11 22:43:17 +0600

    Create GlobalVariables.c

---
+++ Hangman Code/GlobalVariables.c
@@ -0,0 +1,5 @@
+int gameCount = 1, remainingTime, tries = 0, correctGuessCount = 0, themeChoice, wordIndex, hintCount = 0, streak = 0, highestScore = 0, wins = 0, streakBroken = 0;
+bool hintTaken, gameOver, isGuest = false, timeChallenge = false;
+const char *word;
+char guessedLetter, guessed[MAX_LENGTH], currentPlayer[MAX_LENGTH];
+time_t startTime, currentTime;
