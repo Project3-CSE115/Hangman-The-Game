fcdc19fac344bae1140a2fda715e134262bc3a97
Priyonti Tabassum Haque <priyonti.haque.251@northsouth.edu>
2025-04-11 22:27:09 +0600

    Update createAccount().c

--- Hangman Code/createAccount().c
+++ Hangman Code/createAccount().c
@@ -12,9 +12,20 @@ void createAccount() {
         printf(PURPLE "\nEnter player name: " RESET);
         scanf("%s", playerName);
     }
-
+    strcpy(currentPlayer, playerName);
     FILE *file1 = fopen("players.txt","a");
+    if (file1 == NULL) {
+        printf(RED "\nError opening this file!\n" RESET);
+        return;
+    }
     fprintf(file1, "%s\n", playerName);
     fclose(file1);
+    FILE *file2 = fopen("stats.txt","a");
+    if (file2 == NULL) {
+        printf(RED "\nError opening file!\n" RESET);
+        return;
+    }
+    fprintf(file1, "%s %d %d %d %d %d\n", playerName, streak, highestScore, 1, gameCount, streakBroken);
+    fclose(file2);
     printf(GREEN "\nAccount Created Successfully!\n" RESET);
 }
