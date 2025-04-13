84b2251a0ebfb3587bc13ade3d31876c05c9783d
Priyonti Tabassum Haque <priyonti.haque.251@northsouth.edu>
2025-04-13 14:50:51 +0600

    Update and rename Hangman Code/saveProgress().c to Hangman Code/Priyonti/saveProgress().c

--- Hangman Code/Priyonti/saveProgress().c
+++ Hangman Code/Priyonti/saveProgress().c
@@ -8,11 +8,6 @@ void saveProgress() {
         printf(RED "\nError opening file!\n" RESET);
         return;
     }
-
-    printf(GREEN "\nSaving progress for player: %s\n" RESET, currentPlayer);
-    printf(GREEN "Streak: %d, Highest Score: %d, Level: %d, Game: %d, Streak Broken: %d\n" RESET,
-           streak, highestScore, levelCount, gameCount, streakBroken);
-
     while (fscanf(file, "%s %d %d %d %d %d", tempName, &tempStreak, &tempScore, &tempLevel, &tempGame, &tempBroken) != EOF) {
         if (strcmp(tempName, currentPlayer) == 0) {
             fprintf(tempFile, "%s %d %d %d %d %d\n", currentPlayer, streak, highestScore, levelCount, gameCount, streakBroken);
