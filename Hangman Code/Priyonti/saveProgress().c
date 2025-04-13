c70d51736df895c6f0fdb7613188b2f8bfd211db
Priyonti Tabassum Haque <priyonti.haque.251@northsouth.edu>
2025-04-13 23:39:05 +0600

    Update saveProgress().c

--- Hangman Code/Priyonti/saveProgress().c
+++ Hangman Code/Priyonti/saveProgress().c
@@ -1,6 +1,6 @@
 void saveProgress() {
     char tempName[MAX_LENGTH];
-    int tempStreak, tempScore, tempLevel, tempGame, tempBroken;
+    int tempStreak, tempScore, tempLevel, tempGame, tempBroken, tempB1, tempB2, tempB3;
 
     FILE *file = fopen("stats.txt", "r");
     FILE *tempFile = fopen("temp.txt", "w");
@@ -8,11 +8,12 @@ void saveProgress() {
         printf(RED "\nError opening file!\n" RESET);
         return;
     }
-    while (fscanf(file, "%s %d %d %d %d %d", tempName, &tempStreak, &tempScore, &tempLevel, &tempGame, &tempBroken) != EOF) {
-        if (strcmp(tempName, currentPlayer) == 0) {
-            fprintf(tempFile, "%s %d %d %d %d %d\n", currentPlayer, streak, highestScore, levelCount, gameCount, streakBroken);
+
+    while (fscanf(file, "%s %d %d %d %d %d %d %d %d", tempName, &tempStreak, &tempScore, &tempLevel, &tempGame, &tempBroken, &tempB1, &tempB2, &tempB3) != EOF) {
+        if (strcmp(tempName, player.name) == 0) {
+            fprintf(tempFile, "%s %d %d %d %d %d %d %d %d\n", player.name, player.streak, player.highScore, player.level, player.game, player.streakBroken, player.hintBadge, player.speedBadge, player.streakBadge);
         } else {
-            fprintf(tempFile, "%s %d %d %d %d %d\n", tempName, tempStreak, tempScore, tempLevel, tempGame, tempBroken);
+            fprintf(tempFile, "%s %d %d %d %d %d %d %d %d\n", tempName, tempStreak, tempScore, tempLevel, tempGame, tempBroken, tempB1, tempB2, tempB3);
         }
     }
 
