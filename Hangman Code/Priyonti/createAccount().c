13c7d2a591a7c0defacefa3ccd609ef4e251fde5
Priyonti Tabassum Haque <priyonti.haque.251@northsouth.edu>
2025-04-13 23:35:14 +0600

    Update createAccount().c

--- Hangman Code/Priyonti/createAccount().c
+++ Hangman Code/Priyonti/createAccount().c
@@ -1,5 +1,5 @@
 void createAccount() {
-    char playerName[MAX_LENGTH];
+    char playerName[MAX_LENGTH], dob[11];
 
     printf(PURPLE "\nEnter player name: " RESET);
     scanf("%s", playerName);
@@ -12,20 +12,27 @@ void createAccount() {
         printf(PURPLE "\nEnter player name: " RESET);
         scanf("%s", playerName);
     }
-    strcpy(currentPlayer, playerName);
+    printf(PURPLE "\nEnter date of birth(DD/MM/YYYY): " RESET);
+    scanf("%s", dob);
+    while (!isDobValid(dob)) {
+        printf(RED "\nInalid dob format.\n" RESET);
+        printf(PURPLE "\nEnter date of birth(DD/MM/YYYY): " RESET);
+        scanf("%s", dob);
+    }
+    strcpy(player.name, playerName);
     FILE *file1 = fopen("players.txt","a");
     if (file1 == NULL) {
         printf(RED "\nError opening this file!\n" RESET);
         return;
     }
-    fprintf(file1, "%s\n", playerName);
+    fprintf(file1, "%s %d\n", player.name, player.age);
     fclose(file1);
     FILE *file2 = fopen("stats.txt","a");
     if (file2 == NULL) {
         printf(RED "\nError opening file!\n" RESET);
         return;
     }
-    fprintf(file2, "%s %d %d %d %d %d\n", playerName, streak, highestScore, 1, gameCount, streakBroken);
+    fprintf(file2, "%s %d %d %d %d %d %d %d %d\n", player.name, player.streak, player.highScore, 1, 1, 0, 0, 0, 0);
     fclose(file2);
     printf(GREEN "\nAccount Created Successfully!\n" RESET);
 }
